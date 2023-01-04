4c16d99998b0608539f215d15591133bbf29dd12
Rauane Lima <rauanelima7@gmail.com>
2023-01-04 14:23:47 -0300

    Componente de upload das imagens

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import Button from "../components/Button";
 import Avatar from "../components/Avatar";
+import {UploadImagem} from "../components/UploadImagem";
+import { useState, useRef } from "react";
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+
   return (
     <>
-      <h1>Olá mundo</h1>
-      <div style={{ width: 350 }}>
-        <Avatar/>
+      <h1>Ola mundo!</h1>
+      <button onClick={() => referenciaInput?.current?.click()}>abrir seletor de arquivos</button>
+
+      <UploadImagem
+        setImagem={setImagem}
+        imagemPreview={imagem?.preview}
+        aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+      />
+
+      <div style={{width: 200}}>
+        <Avatar />
         <Button
           texto={"Editar perfil"}
           manipularClick={() => console.log("clicou")}
         />
       </div>
     </>
-  );
+  )
 }
