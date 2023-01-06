d87c86de94ecbf8b6f7d30b16419d5c9cf36fdfc
Rauane Lima <rauanelima7@gmail.com>
2023-01-06 11:40:00 -0300

    Estrtura mobile da tela de login

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,18 +1,49 @@
 import InputPublico from "../InputPublico";
-import Envelope from '../../public/Imagens/envelope.svg';
+import { useState } from "react";
+import Envelope from "../../public/Imagens/envelope.svg";
+import Chave from "../../public/Imagens/chave.svg";
+import Logo from "../../public/Imagens/logo.svg";
+import Image from "next/image";
+import Button from "../Button";
+import Link from "next/link";
+
 
 export default function Login() {
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+
   return (
     <section className={"login__Section paginaPublica"}>
-      <div className="logo__Container"></div>
+      <div className="logo__Container">
+        <Image src={Logo} alt={"Logotipo Devagram"} layout="fill" />
+      </div>
       <div className="conteudo__paginaPublica">
         <form>
-          <InputPublico 
-          image={Envelope}
-          texto="Digite seu E-mail..."
-          tipo="email"
-          aoAlterarValor={() => console.log("Digitando email...")}/> 
+          <InputPublico
+            image={Envelope}
+            texto="Digite seu E-mail..."
+            tipo="email"
+            aoAlterarValor={e => setEmail(e.target.value)}
+            valor={email}
+          />
+
+          <InputPublico
+            image={Chave}
+            texto="Digite sua Senha..."
+            tipo="password"
+            aoAlterarValor={e => setSenha(e.target.value)}
+            valor={senha}
+          />
+
+          <Button texto="Login" tipo="submit" disabilitado={false} />
         </form>
+
+        <div className="rodape_paginaPublica">
+          <p>Não possui uma conta?</p>
+          <Link href="/Cadastro">Faça seu cadastro agora!</Link>
+        </div>
       </div>
     </section>
   );
